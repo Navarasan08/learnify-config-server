7eb402f01612edf18ad44d84f8c967e81f60277d
Navarasan <navarasaneee@gmail.com>
2025-05-14 09:44:33 +0530

    Initial commit

---
+++ src/main/java/com/example/learnify_config_server/LearnifyConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnify_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnifyConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnifyConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/learnify_config_server/LearnifyConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnify_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnifyConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
